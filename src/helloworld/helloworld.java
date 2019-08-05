40d6d1cbb80029102463158452d96155dc8d5646
defaultuser0 <defaultuser0@DESKTOP-FDVUPSA>
2019-08-05 20:08:35 +0900

    brunch test

--- src/helloworld/helloworld.java
+++ src/helloworld/helloworld.java
@@ -7,6 +7,5 @@ public class helloworld {
 		System.out.println("hello world");
 		System.out.println("hello world");
 		System.out.println("hello world");
-		System.out.println("hello world");
 	}
 }
