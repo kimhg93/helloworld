20dbef7bac0f3dd420b8421a474457d088f29650
defaultuser0 <defaultuser0@DESKTOP-FDVUPSA>
2019-08-05 17:52:01 +0900

    test commit

--- src/helloworld/helloworld.java
+++ src/helloworld/helloworld.java
@@ -1,7 +1,12 @@
 package helloworld;
 
 public class helloworld {
-	public static void main(String args[]) {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
 		System.out.println("hello world");
 	}
 }
